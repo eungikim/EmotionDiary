39bef7ff9d50b17a19f91809b755d14e27b0c612
eungikim <eungikim.me@gmail.com>
2022-04-22 13:12:41 +0900

    Show wrote diary under the editor
    Using dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "오늘도 쓴다.",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "가람이",
+    content: "오늘도 쓴다.",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "양세훈",
+    content: "오늘도 쓴다.",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "신정환",
+    content: "오늘도 쓴다.",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: "이정환",
+    content: "오늘도 쓴다.",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
