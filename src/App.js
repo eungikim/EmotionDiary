ca65859c171a5f9b6a9551fb45d21ad607a75185
eungikim <eungikim.me@gmail.com>
2022-04-22 17:13:38 +0900

    Get data and init diary list
    Using comments data of json place holder api then initialize diary list data.
    Remove lifecycle component and dummy data.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,35 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "이정환",
-    content: "오늘도 쓴다.",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "가람이",
-    content: "오늘도 쓴다.",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "양세훈",
-    content: "오늘도 쓴다.",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "신정환",
-    content: "오늘도 쓴다.",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 5,
-    author: "이정환",
-    content: "오늘도 쓴다.",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-];
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    console.log("mounT!");
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -76,7 +60,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
